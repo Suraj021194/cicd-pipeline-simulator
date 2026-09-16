6157ade333335e78dfa7c3310a87a73aa6ef34d1
Suraj <surajsrivastava021194@gmail.com>
2026-09-16 23:47:10 +0530

    again modified pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -16,7 +16,7 @@ LOG_FILE="$SCRIPT_DIR/pipeline.log"
 TIMESTAMP=$(date +'%Y-%m-%d_%H-%M-%S')
 ENV="development" #default environment
 STAGE=""
-APP_DIR="$SCRIPT_DIR/myapp"
+APP_DIR="$SCRIPT_DIR/app/myapp"
 
 usage(){
     # print usgae instructions for --stage, --env, --help
